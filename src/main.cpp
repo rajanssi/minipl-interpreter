3724587a6a1722dc603a816b0dd71ff998c9b85e
Anssi Rajala <ajrajal4@gmail.com>
2023-01-28 21:24:45 +0200

    scanner read string literals

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,27 @@
 #include <ostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
+
+enum TokenType {
+  STRING,
+
+};
+
+struct Token {
+  Token(TokenType kind, const std::string lexeme)
+      : kind{kind}, lexeme{lexeme} {}
+  const std::string lexeme;
+  const TokenType kind;
+
+  void print() { std::cout << "{" << kind << ", " << lexeme << "}"; }
+};
 
 class Scanner {
 public:
+  std::vector<std::string> strings;
+  std::vector<Token> tokens;
+
   Scanner(const std::string &fileName) {
     try {
       readSourceFile(fileName);
@@ -21,42 +39,61 @@ public:
   void printChar(const char printable);
   void printSourceString();
 
-  void scanString() {
-    while (currentChar <= sourceString.length()) {
-      printChar(getNextChar());
-      std::cin.get();
+  void scanSource() {
+    while (currentPosition <= sourceString.length()) {
+      tokenStart = currentPosition;
+      scanToken();
     }
-
   }
 
 private:
   std::string sourceString;
 
   int tokenStart = 0;
-  int currentChar = 0;
+  int currentPosition = 0;
   int line = 1;
 
   void readSourceFile(const std::string &fileName);
   void scanToken();
+  void scanString();
+  void addToken(TokenType kind, const std::string value);
   const char getNextChar();
 };
 
-const char Scanner::getNextChar() {
-  return sourceString[currentChar++];
+const char Scanner::getNextChar() { return sourceString[currentPosition++]; }
+
+void Scanner::addToken(TokenType kind, const std::string value) {
+  tokens.push_back({kind, value});
 }
 
 void Scanner::scanToken() {
-
+  char character = getNextChar();
+  switch (character) {
+  case '"':
+    scanString();
+  default:
+    break;
+  }
 }
 
-void Scanner::printChar(const char printable) {
-  std::cout << printable << '\r';
+void Scanner::scanString() {
+  char nextChar = ' ';
+  while (nextChar != '"' && currentPosition <= sourceString.length()) {
+    nextChar = getNextChar();
+    if (nextChar == '\\') {
+      currentPosition++;
+    }
+  }
+
+  addToken(TokenType::STRING, sourceString.substr(tokenStart, currentPosition - tokenStart));
+}
+
+void Scanner::printChar(const char character) {
+  std::cout << character << '\r';
   std::cout.flush();
 }
 
-void Scanner::printSourceString() {
-  std::cout << sourceString << '\n';
-}
+void Scanner::printSourceString() { std::cout << sourceString << '\n'; }
 
 void Scanner::readSourceFile(const std::string &fileName) {
   std::ifstream file(fileName);
@@ -91,7 +128,11 @@ int main(int argc, char *argv[]) {
     return 65;
   }
 
-  scanner->scanString();
+  scanner->scanSource();
+  for (auto t : scanner->tokens) {
+    t.print();
+    std::cout << '\n';
+  }
 
   return 0;
 }
